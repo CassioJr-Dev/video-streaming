0895c604f2feefe75ecbe6f98c0a606922e63bb4
Cássio <cassiojr0108@gmail.com>
2025-11-06 20:50:26 -0300

    decoupling dependency - DAO and dependency inversion

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,20 @@ import { ContentController } from './http/rest/controller/content.controller';
 import { PrismaService } from './persistence/prisma/prisma.service';
 import { ContentManagementService } from './core/service/content-management.service';
 import { MediaPlayerService } from './core/service/media-player.service';
+import { VideoDAOImpl } from './persistence/dao/video.dao';
+import { VideoDAO } from './core/dao/video.dao.interface';
 
 @Module({
   imports: [],
   controllers: [ContentController],
-  providers: [PrismaService, ContentManagementService, MediaPlayerService],
+  providers: [
+    PrismaService,
+    ContentManagementService,
+    MediaPlayerService,
+    {
+      provide: VideoDAO,
+      useClass: VideoDAOImpl,
+    },
+  ],
 })
 export class AppModule {}
